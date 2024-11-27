0004fa810d6829fb60f31eeea734cfff2b2338fb
Ariel Navas <arielnavas05@gmail.com>
2024-11-27 10:59:15 -0500

    F

--- src/controller/aes.controller.ts
+++ src/controller/aes.controller.ts
@@ -1,12 +1,21 @@
-export function createState(input: string): number[][] {
-    const state: number[][] = [];
+
+/**
+ * Crea la matriz de estado 4x4 para AES a partir de una cadena.
+ * @param input Cadena de texto (mensaje o clave).
+ * @param asHex Si es `true`, convierte los valores en hexadecimal.
+ * @returns Matriz 4x4 que representa el estado inicial.
+ */
+export function createState(input: string, asHex:boolean=false): string[][] |number[][] {
+    const state: number[][] | string[][] = [];
     const paddedInput = input.padEnd(16, '\0') //relleno con \0 si es menor que 16 bytes
     for (let i = 0; i < 4; i++) {
         state[i] = [];
         for (let j = 0; j < 4; j++) {
-            state[i][j] = paddedInput.charCodeAt(i * 4 + j);
+            const byte= paddedInput.charCodeAt(i * 4 + j);
+            state[i][j] = asHex ? byte.toString(16).padStart(2, '0') : byte;
         }
     }
 
     return state;
 };
+
